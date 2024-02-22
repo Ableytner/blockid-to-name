b7f2b012fe10fad2217f03840750585f7c2af4d7
Ableytner <ableit190030@sr.htlweiz.at>
2024-02-22 03:40:16 +0100

    add functionality

--- src/main/java/com/ableytner/bidtoname/BlockIdtoName.java
+++ src/main/java/com/ableytner/bidtoname/BlockIdtoName.java
@@ -1,14 +1,17 @@
 package com.ableytner.bidtoname;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import net.minecraft.block.Block;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
 
 @Mod(
     modid = BlockIdtoName.MODID,
@@ -20,31 +23,33 @@ public class BlockIdtoName {
     public static final String MODID = "mymodid";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.ableytner.bidtoname.ClientProxy", serverSide = "com.ableytner.bidtoname.CommonProxy")
-    public static CommonProxy proxy;
-
     @Mod.EventHandler
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
     // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit(event);
+        LOG.info("I am BlockIdtoName at version " + Tags.VERSION);
     }
 
     @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
-    public void init(FMLInitializationEvent event) {
-        proxy.init(event);
-    }
+    public void worldStart(FMLServerStartedEvent event) {
+        LOG.info("!!! server started !!!");
 
-    @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
-    public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit(event);
-    }
+        JSONObject blocks = new JSONObject();
 
-    @Mod.EventHandler
-    // register server commands in this event handler (Remove if not needed)
-    public void serverStarting(FMLServerStartingEvent event) {
-        proxy.serverStarting(event);
+        for (int id = 0; id < 4096; id++) {
+            Block b = Block.getBlockById(id);
+            if (b != null && b.getUnlocalizedName() != "tile.air") { // check for unlocalized name doesn't work
+                blocks.put(id, b.getUnlocalizedName());
+            }
+        }
+
+        try {
+            FileWriter fw = new FileWriter("blockid_to_name.json");
+            fw.write((blocks.toJSONString()));
+            fw.close();
+            LOG.info("!!! saving finished !!!");
+        } catch (IOException e) {
+            LOG.info("!!! saving failed !!!");
+        }
     }
 }
