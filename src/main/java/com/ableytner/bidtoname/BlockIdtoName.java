fac024fd888a0a5074e82c1698b2d5d05810f31d
Ableytner <ableit190030@sr.htlweiz.at>
2024-02-22 02:37:54 +0100

    setup without functionality

--- src/main/java/com/ableytner/bidtoname/BlockIdtoName.java
+++ src/main/java/com/ableytner/bidtoname/BlockIdtoName.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.ableytner.bidtoname;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,13 +10,17 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "MyMod", acceptedMinecraftVersions = "[1.7.10]")
-public class MyMod {
+@Mod(
+    modid = BlockIdtoName.MODID,
+    version = Tags.VERSION,
+    name = "BlockIdtoName",
+    acceptedMinecraftVersions = "[1.7.10]")
+public class BlockIdtoName {
 
     public static final String MODID = "mymodid";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.myname.mymodid.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
+    @SidedProxy(clientSide = "com.ableytner.bidtoname.ClientProxy", serverSide = "com.ableytner.bidtoname.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
